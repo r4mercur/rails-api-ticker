7d9a06d4ebd7284a718bcf720be6fd46cb4ec82d
Bjarne Bräuer <bjarne.braeuer@sportplatz-media.com>
2023-11-26 19:57:27 +0100

    added new datatype competition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_25_220127) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_26_185421) do
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
