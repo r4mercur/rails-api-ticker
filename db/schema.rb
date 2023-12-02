587e49e95ab72925b908564a13152da1831db139
Bjarne Bräuer <bjarne.braeuer@sportplatz-media.com>
2023-12-02 14:11:00 +0100

    create ticker model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_202448) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_02_122928) do
   create_table "competitions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -60,6 +60,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_202448) do
     t.string "shortname"
   end
 
+  create_table "tickers", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "date"
+    t.integer "goals_home"
+    t.integer "goals_away"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_tickers_on_game_id"
+    t.index ["user_id"], name: "index_tickers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
@@ -73,4 +85,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_202448) do
   add_foreign_key "participations", "competitions"
   add_foreign_key "participations", "teams"
   add_foreign_key "players", "teams"
+  add_foreign_key "tickers", "games"
+  add_foreign_key "tickers", "users"
 end
