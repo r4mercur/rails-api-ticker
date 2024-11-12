2f52da984c5139a950ed398de2889cd01102631e
Bjarne Braeuer <bjarne.braeuer@parshipgroup.com>
2024-11-12 14:02:50 +0100

    fix data seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -11,20 +13,33 @@
 require 'faker'
 require 'json'
 
-# Competitions
-competitions_file = File.read(Rails.root.join('db', 'datasets', 'competitions.json'))
-competitions_data = JSON.parse(competitions_file)
+# Get data from JSON files
+def get_data_from_json(file_name)
+  file = File.read(Rails.root.join('db', 'datasets', file_name))
+  JSON.parse(file)
+end
 
+# Competitions
+competitions_data = get_data_from_json('competitions.json')
 competitions_data['competitions'].each do |competition|
   Competition.find_or_create_by!(name: competition['name'])
 end
 
 # Teams
-teams_file = File.read(Rails.root.join('db', 'datasets', 'teams.json'))
-teams_data = JSON.parse(teams_file)
-
+teams_data = get_data_from_json('teams.json')
 teams_data['teams'].each do |team|
-  Team.find_or_create_by!(name: team['name'], shortname: team['shortname'])
+  Team.find_or_create_by!(name: team['name'], shortname: team['shortName'])
+end
+
+# Players
+players_data = get_data_from_json('players.json')
+players_data['players'].each do |player|
+  Player.find_or_create_by!(
+    name: player['name'],
+    age: player['age'],
+    number: player['number'],
+    position: player['position']
+  )
 end
 
 # Users
