298533ddb68810e4359b0fc40f4ec407cca94001
r4mercur <bjarnmb9@gmail.com>
2024-11-17 13:41:25 +0100

    make password input secret

--- db/superuser.rb
+++ db/superuser.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'io/console'
+
 def create_super_user(email, password)
   user = User.new(email: email, password: password, password_confirmation: password)
   user.is_admin = true
@@ -13,6 +15,6 @@ puts "Please enter the email address for the super user:"
 email = gets.chomp
 
 puts "Please enter the password for the super user:"
-password = gets.chomp
+password = STDIN.noecho(&:gets).chomp
 
 create_super_user(email, password)
